94a85df1d4d9287d48d4c2751f921f40ee72a340
Raju Ahmmed <rajuahmmedbd@gmail.com>
2022-08-04 11:29:25 +0600

    feat(D5, 3PS) :: extension zip process

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest } = require( 'gulp' );
+const gulpZip = require( 'gulp-zip' );
+const package = require('./package.json');
+
+const files = [
+    '**/*',
+
+    // Ignored folders.
+    '!**/.*/**', // Hidden files/dirs on Mac/Linux
+    '!**/__*/**', // Hidden dirs on Mac
+    '!.yarn/**',
+    '!node_modules/**',
+    '!src/**',
+    '!test-config/**',
+
+    // Ignored files.
+    '!**/*.zip',
+    '!**/*.map',
+    '!.gitignore',
+    '!.yarnrc.yml',
+    '!gulpfile.js',
+    '!package.json',
+    '!tsconfig.json',
+    '!webpack.config.js',
+    '!yarn.lock',
+];
+
+const zip = () => src( files ).pipe( gulpZip( package.name + '-v' + package.version + '.zip' ) ).pipe( dest( './' ) );
+
+exports.zip = zip;
