f5b3d8bcc772e92e7507ed87f3310669caa52b0b
Raju Ahmmed <rajuahmmedbd@gmail.com>
2022-10-08 13:03:44 +0600

    fix(D5, 3PS) :: prefix added for hooks

--- src/conversion.ts
+++ src/conversion.ts
@@ -1,13 +1,13 @@
 import { addFilter } from '@wordpress/hooks';
 
-addFilter('custom_modules_list', 'd5-module-extension-example', (modulesName) => {
+addFilter('et_vb_custom_modules_list', 'd5-module-extension-example', (modulesName) => {
   return {
       ...modulesName,
       d4_module: 'divi/d4-module',
   };
 });
 
-addFilter('module_exceptions_attr_map', 'd5-module-extension-example', (moduleExceptions) => {
+addFilter('et_vb_module_exceptions_attr_map', 'd5-module-extension-example', (moduleExceptions) => {
   return {
       ...moduleExceptions,
       'd4-module': {
