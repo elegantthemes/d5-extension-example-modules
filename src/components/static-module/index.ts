94662f342fc4ea175e2810d032533ed719721856
mohmed98 <mohamedatta447@gmail.com>
2025-10-22 18:07:13 +0300

    feat(Static Module): Add default printed style attributes and update module definition
    - Introduced `module-default-printed-style-attributes.json` to manage printed style attributes for the static module.
    - Updated `index.ts` to import and utilize the new printed style attributes alongside existing default attributes.
    - Enhanced the static module definition for improved modularity and maintainability.

--- src/components/static-module/index.ts
+++ src/components/static-module/index.ts
@@ -7,6 +7,7 @@ import {
 // Local dependencies.
 import metadata from './module.json';
 import defaultRenderAttributes from './module-default-render-attributes.json';
+import defaultPrintedStyleAttributes from './module-default-printed-style-attributes.json';
 import { StaticModuleEdit } from './edit';
 import { StaticModuleAttrs } from './types';
 import { placeholderContent } from './placeholder-content';
@@ -17,8 +18,9 @@ import './module.scss';
 
 export const staticModule: ModuleLibrary.Module.RegisterDefinition<StaticModuleAttrs> = {
   // Imported json has no inferred type hence type-cast is necessary.
-  metadata: metadata as Metadata.Values<StaticModuleAttrs>,
-  defaultAttrs: defaultRenderAttributes as Metadata.DefaultAttributes<StaticModuleAttrs>,
+  metadata:                 metadata as Metadata.Values<StaticModuleAttrs>,
+  defaultAttrs:             defaultRenderAttributes as Metadata.DefaultAttributes<StaticModuleAttrs>,
+  defaultPrintedStyleAttrs: defaultPrintedStyleAttributes as Metadata.DefaultAttributes<StaticModuleAttrs>,
   placeholderContent,
   renderers: {
     edit: StaticModuleEdit,
