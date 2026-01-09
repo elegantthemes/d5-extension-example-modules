7ee46aaaa9e8ae8b4a52bb16deddcd7546ef7bd3
Ayub Adiputra <ayubna32@gmail.com>
2026-01-10 00:01:25 +0700

    fix(D5, Child Module, Parent Attributes): Update ChildModule example to use correct parent attribute pattern
    ChildModule was using `$parent->attrs` which only contains raw attributes,
    missing defaults and presets. Updated to use `ModuleUtils::get_all_attrs()`
    matching the pattern used by all Divi core child modules.
    
    Also removed redundant manual attribute merging in `ModuleStylesTrait`
    since `parentAttrs` received via `$args` is already fully merged.
    
    This ensures the example module demonstrates the correct pattern for
    third-party developers and allows child modules to access parent
    default values.
    
    Fixes https://github.com/elegantthemes/Divi/issues/46705

--- modules/ChildModule/ChildModuleTrait/RenderCallbackTrait.php
+++ modules/ChildModule/ChildModuleTrait/RenderCallbackTrait.php
@@ -20,11 +20,13 @@ use ET\Builder\FrontEnd\BlockParser\BlockParserStore;
 use ET\Builder\Packages\IconLibrary\IconFont\Utils;
 use ET\Builder\Packages\Module\Options\Element\ElementComponents;
 use ET\Builder\Packages\ModuleLibrary\ModuleRegistration;
+use ET\Builder\Packages\ModuleUtils\ModuleUtils;
 use MEE\Modules\ChildModule\ChildModule;
 
 trait RenderCallbackTrait {
 	use ModuleClassnamesTrait;
 	use ModuleStylesTrait;
+	use ModuleScriptDataTrait;
 
 	/**
 	 * Child module render callback which outputs server side rendered HTML on the Front-End.
@@ -41,13 +43,10 @@ trait RenderCallbackTrait {
 	public static function render_callback( $attrs, $content, $block, $elements ) {
 		$parent = BlockParserStore::get_parent( $block->parsed_block['id'], $block->parsed_block['storeInstance'] );
 
-		$parent_attrs = $parent->attrs ?? [];
-
-		$parent_default_attributes = ModuleRegistration::get_default_attrs( 'example/parent-module' );
-		$parent_attrs_with_default = array_replace_recursive( $parent_default_attributes, $parent_attrs );
+		$parent_attrs = ModuleUtils::get_all_attrs( $parent );
 
 		// Icon.
-		$icon_value = $attrs['icon']['innerContent']['desktop']['value'] ?? $parent_attrs_with_default['icon']['innerContent']['desktop']['value'] ?? [];
+		$icon_value = $attrs['icon']['innerContent']['desktop']['value'] ?? $parent_attrs['icon']['innerContent']['desktop']['value'] ?? [];
 		$icon       = HTMLUtility::render(
 			[
 				'tag'               => 'div',
@@ -90,21 +89,22 @@ trait RenderCallbackTrait {
 		return Module::render(
 			[
 				// FE only.
-				'orderIndex'         => $block->parsed_block['orderIndex'],
-				'storeInstance'      => $block->parsed_block['storeInstance'],
+				'orderIndex'          => $block->parsed_block['orderIndex'],
+				'storeInstance'       => $block->parsed_block['storeInstance'],
 
 				// VB equivalent.
-				'id'                 => $block->parsed_block['id'],
-				'name'               => $block->block_type->name,
-				'moduleCategory'     => $block->block_type->category,
-				'attrs'              => $attrs,
-				'elements'           => $elements,
-				'classnamesFunction' => [ ChildModule::class, 'module_classnames' ],
-				'stylesComponent'    => [ ChildModule::class, 'module_styles' ],
-				'parentAttrs'        => $parent_attrs,
-				'parentId'           => $parent->id ?? '',
-				'parentName'         => $parent->blockName ?? '',
-				'children'           => ElementComponents::component(
+				'id'                  => $block->parsed_block['id'],
+				'name'                => $block->block_type->name,
+				'moduleCategory'      => $block->block_type->category,
+				'attrs'               => $attrs,
+				'elements'            => $elements,
+				'classnamesFunction'  => [ ChildModule::class, 'module_classnames' ],
+				'stylesComponent'     => [ ChildModule::class, 'module_styles' ],
+				'scriptDataComponent' => [ ChildModule::class, 'module_script_data' ],
+				'parentAttrs'         => $parent_attrs,
+				'parentId'            => $parent->id ?? '',
+				'parentName'          => $parent->blockName ?? '',
+				'children'            => ElementComponents::component(
 					[
 						'attrs'         => $attrs['module']['decoration'] ?? [],
 						'id'            => $block->parsed_block['id'],
